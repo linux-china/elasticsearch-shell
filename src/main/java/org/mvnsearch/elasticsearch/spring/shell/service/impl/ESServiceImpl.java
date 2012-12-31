99413151572780adf24c30b03d0cf7fd40a2cbe4
Jacky Chan <libing.chen@gmail.com>
2012-12-31 17:15:23 +0800

    ES service implementation

---
+++ src/main/java/org/mvnsearch/elasticsearch/spring/shell/service/impl/ESServiceImpl.java
@@ -0,0 +1,53 @@
+package org.mvnsearch.elasticsearch.spring.shell.service.impl;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.mvnsearch.elasticsearch.spring.shell.service.ESService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * ES service implementation
+ *
+ * @author linux_china
+ */
+@Component("eSService")
+public class ESServiceImpl implements ESService {
+    /**
+     * es client
+     */
+    private TransportClient esClient;
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+    /**
+     * connect
+     *
+     * @param host host
+     * @param port port
+     */
+    public void connect(String host, Integer port) {
+        esClient = new TransportClient();
+        esClient.addTransportAddress(new InetSocketTransportAddress(host, port));
+    }
+
+    /**
+     * get source
+     *
+     * @param index index name
+     * @param type  type name
+     * @param id    id
+     * @return source
+     */
+    public String getSource(String index, String type, String id) {
+        GetResponse response = esClient.prepareGet(index, type, id)
+                .execute()
+                .actionGet();
+        return response.getSourceAsString();
+    }
+}
