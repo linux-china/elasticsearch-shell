7d934968d62467000718d61054b3779b93b7a258
Jacky Chan <libing.chen@gmail.com>
2012-12-31 17:05:20 +0800

    ES constants

---
+++ src/main/java/org/mvnsearch/elasticsearch/spring/shell/service/ESConstants.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.elasticsearch.spring.shell.service;
+
+/**
+ * ES constants
+ *
+ * @author linux_china
+ */
+public class ESConstants {
+    /**
+     * node
+     */
+    public static String node;
+    /**
+     * index
+     */
+    public static String index;
+    /**
+     * type
+     */
+    public static String type;
+}
