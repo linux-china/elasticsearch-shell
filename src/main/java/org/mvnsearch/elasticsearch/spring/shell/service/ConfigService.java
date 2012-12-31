6f2056d1dec20bdf6b4200e0f3160fd5a76660a5
Jacky Chan <libing.chen@gmail.com>
2012-12-31 16:36:13 +0800

    config service

---
+++ src/main/java/org/mvnsearch/elasticsearch/spring/shell/service/ConfigService.java
@@ -0,0 +1,59 @@
+package org.mvnsearch.elasticsearch.spring.shell.service;
+
+/**
+ * config service
+ *
+ * @author linux_china
+ */
+public interface ConfigService {
+    /**
+     * available
+     *
+     * @return available indicat
+     */
+    public boolean available();
+
+    /**
+     * set node
+     *
+     * @param node node
+     */
+    public void setNode(String node);
+
+    /**
+     * get node
+     *
+     * @return node
+     */
+    public String getNode();
+
+    /**
+     * set index
+     *
+     * @param index index name
+     */
+    public void setIndex(String index);
+
+    /**
+     * get index name
+     *
+     * @return index name
+     */
+    public String getIndex();
+
+    /**
+     * set type
+     *
+     * @param type type name
+     */
+    public void setType(String type);
+
+    /**
+     * get type
+     *
+     * @param type type
+     * @return type
+     */
+    public String getType(String type);
+
+}
